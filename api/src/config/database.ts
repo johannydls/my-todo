cbee21239e3d94257002bd5fff34b4fb1839edb6
Johanny <johanny.dls@gmail.com>
2022-07-17 16:18:08 -0300

    Add DB_URL nas variáveis de ambiente

--- api/src/config/database.ts
+++ api/src/config/database.ts
@@ -9,8 +9,9 @@ async function main() {
   const DB_USER = process.env.DB_USER || 'user';
   const DB_PASS = process.env.DB_PASS || 'pass';
   const DB_NAME = process.env.DB_NAME || '';
+  const DB_URL = process.env.DB_URL || 'url'
 
-  const URL = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.y7uig.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+  const URL = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_URL}/${DB_NAME}?retryWrites=true&w=majority`;
   console.log(`\nConnecting database...`);
   await mongoose.connect(URL).then(() => {
     console.log(`Database connected ${new Date(Date.now())}\n`);
