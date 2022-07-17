00f5b04152818b73f8b111b10f29c5a3f553033b
Johanny <johanny.dls@gmail.com>
2022-07-17 16:00:11 -0300

    Add rota de listagem de tasks

---
+++ api/src/app/controllers/task.controller.ts
@@ -0,0 +1,33 @@
+import { Express, Router, Request, Response } from 'express';
+import { StatusCodes, ReasonPhrases } from 'http-status-codes';
+
+import { Task } from '../models/task.model';
+
+export function controller(app: Express): void {
+  console.log('::: Loading task.controller.ts - base route: /api/task :::');
+
+  const router = Router();
+
+  router.get('/list', async (req: Request, res: Response) => {
+    try {
+      const paginate_options = {
+        query: {},
+        limit: 10,
+        page: 1,
+        populate: [
+          { path: 'owner', select: 'email' }
+        ]
+      }
+      const tasks = await Task.paginate(paginate_options);
+
+      return res.send(tasks);
+    } catch (error) {
+      console.log(error);
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ 
+        message: ReasonPhrases.INTERNAL_SERVER_ERROR
+      });
+    }
+  });
+
+  app.use('/api/task', router);
+}
