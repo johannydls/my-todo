f8e1e98fadcd25772168fabd237c480199c7931e
Johanny <johanny.dls@gmail.com>
2022-07-17 15:21:20 -0300

    Adiciona rota de autenticação de usuário

--- api/src/app/controllers/user.controller.ts
+++ api/src/app/controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import { Express, Router, Request, Response } from "express";
 import { StatusCodes, ReasonPhrases } from "http-status-codes";
+import { compare as bcryptCompare } from 'bcrypt';
 
 import { User } from "../models/user.model";
 
@@ -35,6 +36,37 @@ export function controller (app: Express): void {
       user = await User.create(body);
       user.password = undefined;
       return res.send(user);
+    } catch (error) {
+      console.log(error);
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ 
+        message: ReasonPhrases.INTERNAL_SERVER_ERROR
+      });
+    }
+  });
+
+  router.post('/authenticate', async (req: Request, res: Response) => {
+    try {
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email }).select('+password');
+
+      if (!user) {
+        return res.status(StatusCodes.NOT_FOUND).send({ 
+          message: ReasonPhrases.NOT_FOUND 
+        });
+      }
+
+      if (!await bcryptCompare(password, user.password || '')) {
+        return res.status(StatusCodes.BAD_REQUEST).send({ 
+          message: 'Invalid password' 
+        }); 
+      }
+
+      user.password = undefined;
+
+      return res.send({
+        user
+      });
     } catch (error) {
       console.log(error);
       return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ 
