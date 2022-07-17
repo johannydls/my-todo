1a7a0adb357ce0b522930a0ca43beaf528df5446
Johanny <johanny.dls@gmail.com>
2022-07-17 14:36:50 -0300

    Adiciona model de user

---
+++ api/src/app/models/user.model.ts
@@ -0,0 +1,46 @@
+import { Schema } from 'mongoose';
+import { hash } from 'bcrypt';
+import mongoosePaginate from 'mongoose-paginate-v2';
+
+import { mongoose }  from '../../config/database';
+
+export interface IUser {
+  name: string;
+  lastname?: string;
+  email: string;
+  is_admin: boolean;
+  password?: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
+
+const UserSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  lastname: { type: String, required: false },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  is_admin: { type: Boolean, required: true, default: false },
+  password: { type: String, required: true, select: false }
+}, {
+  versionKey: false,
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+
+UserSchema.pre('save', async function(next) {
+  try {
+    const pass_hash = await hash(this.password || '', 10);
+    this.password = pass_hash;
+    next();
+  } catch (error) {
+    console.log(`Model: User -> UserSchema.pre('save')`);
+    console.log('Error\n', error);
+  }
+});
+
+UserSchema.plugin(mongoosePaginate);
+
+const User = mongoose.model<IUser>('User', UserSchema);
+
+export { User };
