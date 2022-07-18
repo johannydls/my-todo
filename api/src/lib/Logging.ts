23912b0a99223826c1e86ac0ed0f6c71b876d54f
Johanny <johanny.dls@gmail.com>
2022-07-18 19:08:25 -0300

    Atualiza logging

--- api/src/lib/Logging.ts
+++ api/src/lib/Logging.ts
@@ -4,12 +4,20 @@ export default class Logging {
 
   public static log = (args: any) => this.info(args)
 
+  public static sys = (args: any) => {
+    console.log(chalk.cyan(`[${new Date().toLocaleString()}] [INFO] `), typeof args === 'string' ? chalk.cyanBright(args) : args);
+  }
+
+  public static sysNL = (args: any) => {
+    console.log(`\n${chalk.cyan(`[${new Date().toLocaleString()}] [INFO] `)}`, typeof args === 'string' ? chalk.cyanBright(args) : args);
+  }
+
   public static info = (args: any) => {
     console.log(chalk.blue(`[${new Date().toLocaleString()}] [INFO] `), typeof args === 'string' ? chalk.blueBright(args) : args);
   }
 
   public static infoRequest = (args: any) => {
-    console.log(chalk.green(`[${new Date().toLocaleString()}] [INFO] `), typeof args === 'string' ? chalk.greenBright(args) : args);
+    console.log(chalk.green(`[${new Date().toLocaleString()}] [REQUEST] `), typeof args === 'string' ? chalk.greenBright(args) : args);
   }
 
   public static infoNL = (args: any) => {
