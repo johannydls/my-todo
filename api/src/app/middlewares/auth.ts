779d50a571da3b3fcc2e987c8b92d7431c8cb78b
Johanny <johanny.dls@gmail.com>
2022-07-18 01:32:13 -0300

    Add middleware de autenticação

---
+++ api/src/app/middlewares/auth.ts
@@ -0,0 +1,39 @@
+import { Request } from 'express';
+import { verify as JWT_Verify,  } from 'jsonwebtoken';
+import { StatusCodes, ReasonPhrases } from 'http-status-codes';
+
+type CustomRequest = Request & {
+  user_id: string;
+}
+
+export function auth (req: CustomRequest, res: any, next: any): any {
+  const auth_header = req.headers ? req.headers.authorization : '';
+  // const authCookie = req.cookies ? req.cookies.authorization : '';
+
+  if (!auth_header) {
+    return res.status(StatusCodes.UNAUTHORIZED).send({
+      message: ReasonPhrases.UNAUTHORIZED
+    });
+  }
+
+  const parts = auth_header.split('.');
+
+  if (!(parts.length === 3)) {
+    return res.status(StatusCodes.UNAUTHORIZED).send({ message: 'Token error' });
+  }
+
+  const [part1, part2, part3] = parts;
+  if (!(part1.length === 36)) {
+    return res.status(StatusCodes.UNAUTHORIZED).send({ message: 'Token malformatted' });
+  }
+
+  const decoded = JWT_Verify(auth_header, process.env.JWT_SECRET || '') as any;
+
+  if (decoded) {
+    req.user_id = decoded.id;
+    return next();
+  } else {
+    return res.status(StatusCodes.UNAUTHORIZED).send({ message: 'Invalid token' });
+  }
+
+}
