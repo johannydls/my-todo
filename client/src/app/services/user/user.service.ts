dc3f1ff39179b3e78b613ad9ec599dcc40622af0
Johanny <johanny.dls@gmail.com>
2022-07-20 16:40:56 -0300

    Atualiza service de user com rota de autenticação

--- client/src/app/services/user/user.service.ts
+++ client/src/app/services/user/user.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
+import { environment } from 'src/environments/environment';
+import { SessionService } from '../session/session.service';
+import { LoginForm, AxiosUserAuth } from '../../helpers/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private END_POINT: string;
+
+  constructor(
+    private session: SessionService
+  ) {
+    this.END_POINT = environment.base_url;
+  }
+
+  private getConfig(): any {
+    const token = this.session.getAuthorization();
+    axios.defaults.headers.common['Authorization'] = token;
+    return { headers: { Authorization: token } };
+  }
+
+  public userAuthenticate(data: LoginForm): Promise<AxiosUserAuth> {
+    return axios.post(this.END_POINT + '/user/authenticate', data);
+  }
 }
