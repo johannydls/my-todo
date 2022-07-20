6a844a5fee06ec7db35ec6f339f7508bf13b510e
Johanny <johanny.dls@gmail.com>
2022-07-20 16:41:36 -0300

    Cria arquivo de interfaces e add funções de sessão

---
+++ client/src/app/helpers/interfaces.ts
@@ -0,0 +1,40 @@
+import { AxiosError } from "axios";
+
+export type LoginForm = {
+  email: string;
+  password: string;
+}
+
+export interface UserAuth {
+  token: string;
+  token_expires: string;
+  user: {
+    _id: string;
+    name: string;
+    lastname: string;
+    email: string;
+    is_admin: boolean;
+    created_at: string;
+    updated_at: string
+  }
+}
+
+export interface User {
+  _id: string;
+  name: string;
+  lastname: string;
+  email: string;
+  is_admin: boolean;
+  created_at: string;
+  updated_at: string
+}
+
+export interface AxiosUserAuth {
+  data: UserAuth
+}
+
+export interface AxiosCatch {
+  response: {
+    data: any
+  }
+}
--- client/src/app/services/session/session.service.ts
+++ client/src/app/services/session/session.service.ts
@@ -1,17 +1,85 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/helpers/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
 
-  public navbar: { closed: boolean } = {
+  private AUTHORIZATION: string;
+  private USER_ID: string | undefined;
+
+  public user: User | undefined;
+  public USERNAME: string | undefined;
+  public isLogged: boolean;
+
+
+  public navbar: Navbar = {
     closed: false
   }
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) {
+    this.isLogged = false;
+    this.user = undefined;
+  }
+
+  public logout(): void {
+    console.log('logout session')
+  }
 
   public changeStatusNavbar(status: boolean): void {
     this.navbar.closed = status;
   }
+
+  public getAuthorization(): string {
+    return this.AUTHORIZATION;
+  }
+
+  public setAuthorization(value: string): void {
+    localStorage.setItem('authorization', value);
+    this.AUTHORIZATION = value;
+  }
+
+  public getIsLogged(): boolean {
+    return this.isLogged;
+  }
+
+  public setIsLogged(value: boolean): void {
+    this.isLogged = value;
+  }
+
+  public getUserID(): string | any {
+    this.USER_ID;
+  }
+
+  public setUserID(value: string): void {
+    this.USER_ID = value;
+  }
+
+  public setUserName(value: string): void {
+    this.USERNAME = value;
+  }
+
+  public getUser(): User | undefined {
+    return this.user || undefined;
+  }
+
+  public setUser(data: User): void {
+    this.user = data;
+  }
+
+  public async routeNavigate(route: string, params: any = {}): Promise<boolean> {
+    return this.router.navigate([route], params);
+  }
+
+  public getRouter(): Router {
+    return this.router;
+  }
+}
+
+interface Navbar {
+  closed: boolean;
 }
